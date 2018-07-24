876645c4b32b2f8930b769627a005d39abef71f1
randle7474 <romy7474@gmail.com>
2018-07-23 18:37:29 -0700

    Project 7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,58 @@
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { Platform, NavController, MenuController } from 'ionic-angular';
+import { Splashscreen } from "ionic-native";
+import { StatusBar } from "ionic-native";
+import firebase from 'firebase';
 
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { ListPage } from "../pages/list/list";
+import { DetailPage } from "../pages/detail/detail"
+import { HomePage } from "../pages/home/home";
+import { AuthService } from "../services/auth";
+
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  @ViewChild(Nav) nav: Nav;
-
   rootPage: any = HomePage;
+  signinPage = HomePage;
+  signupPage = ListPage;
+  isAuthenticated = false;
+  @ViewChild('nav') nav: NavController;
 
-  pages: Array<{title: string, component: any}>;
-
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
-    this.initializeApp();
-
-    // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
-    ];
-
-  }
-
-  initializeApp() {
-    this.platform.ready().then(() => {
+  constructor(platform: Platform,
+              private menuCtrl: MenuController,
+              private authService: AuthService) {
+    firebase.initializeApp ({
+      apiKey: "AIzaSyAFeY_prD7aVyIT5iTPCnjtusU_kYBbtgk",
+      authDomain: "myfirstproject-875fd.firebaseapp.com",
+       });
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.isAuthenticated = true;
+        this.rootPage = DetailPage;
+      } else {
+        this.isAuthenticated = false;
+        this.rootPage = ListPage;
+      }
+    });
+    platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
+      StatusBar.styleDefault();
+      Splashscreen.hide();
+        });
   }
 
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+  onLoad(page: any) {
+    this.nav.setRoot(page);
+    this.menuCtrl.close();
+  }
+
+  onLogout() {
+    this.authService.logout();
+    this.menuCtrl.close();
+    this.nav.setRoot(HomePage);
   }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { ListPage } from "../pages/list/list";
+import { HomePage } from "../pages/home/home";
+import { DetailPage } from "../pages/detail/detail";
+import { AuthService } from "../services/auth";
+
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    DetailPage
   ],
   imports: [
-    BrowserModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    DetailPage
   ],
   providers: [
-    StatusBar,
-    SplashScreen,
+    AuthService,
+  
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
